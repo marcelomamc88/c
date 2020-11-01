715f3d57b5aa382092f80b6dbda1e9074c0bdf3a
marcelomamc <marcelomamc@fab.mil.br>
2020-11-01 17:56:05 -0300

    Alteração para Alfabeto Dinâmico.
    Agora é possível criar qualquer alfabeto e não todos os caracteres UNICODE

--- af_match.c
+++ af_match.c
@@ -1,14 +1,18 @@
-#include<stdio.h>
-#include<string.h>
-#define NO_OF_CHARS 256
+#include <stdio.h>
+#include <string.h>
+#define NO_OF_CHARS 3
 
-int getNextState(char *pat, int M, int state, int x)
+char alphabet[NO_OF_CHARS] = {'A', 'B', 'C'};
+
+//entra aqui para cada STATE e cada CHARACTER do alfabeto
+int getNextState(char *pat, int M, int state, char c_alphabet)
 {
     // If the character c is same as next character
     // in pattern,then simply increment state
-    if (state < M && x == pat[state])
+    if (state < M && c_alphabet == pat[state])
         return state+1;
 
+    //NS is Next State
     // ns stores the result which is next state
     int ns, i;
 
@@ -20,7 +24,7 @@ int getNextState(char *pat, int M, int state, int x)
     // is also suffix
     for (ns = state; ns > 0; ns--)
     {
-        if (pat[ns-1] == x)
+        if (pat[ns-1] == c_alphabet)
         {
             for (i = 0; i < ns-1; i++)
                 if (pat[i] != pat[state-ns+1+i])
@@ -33,14 +37,23 @@ int getNextState(char *pat, int M, int state, int x)
     return 0;
 }
 
+int get_char_alphabet(char letter){
+    for (int i=0; i<NO_OF_CHARS; ++i)
+        if (letter == alphabet[i]) return i;
+    
+    return -1;
+}
+
 /* This function builds the TF table which represents4
 	Finite Automata for a given pattern */
 void computeTF(char *pat, int M, int TF[][NO_OF_CHARS])
 {
-    int state, x;
+    int state, c_alphabet;
     for (state = 0; state <= M; ++state)
-        for (x = 0; x < NO_OF_CHARS; ++x)
-            TF[state][x] = getNextState(pat, M, state, x);
+        for (c_alphabet = 0; c_alphabet < NO_OF_CHARS; ++c_alphabet)
+            TF[state][c_alphabet] = getNextState(pat, M, state, alphabet[c_alphabet]);
+
+
 
     printf("TF:\r\n");
     for (int i=0; i<state; ++i){
@@ -65,17 +78,16 @@ void search(char *pat, char *txt)
     int i, state=0;
     for (i = 0; i < N; i++)
     {
-        state = TF[state][txt[i]];
+        state = TF[state][get_char_alphabet(txt[i])];
         if (state == M)
-            printf ("\n Pattern found at index %d",
-                    i-M+1);
+            printf ("\n Pattern found at index %d", i-M+1);
     }
 }
 
 // Driver program to test above function
 int main()
 {
-    char *txt = "AABAACAADAABAAABAA";
+    char *txt = "ABABABACABA";
     char *pat = "ABABACA";
     search(pat, txt);
     return 0;
